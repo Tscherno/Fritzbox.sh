a1a6666eecdb30eb63376eaf2f5f1b1fb821418a
Tscherno <tscherno@gmail.com>
2013-10-06 07:59:01 -0700

    Version 0.5.0 - Aktion "DECT200Energie" hinzugefügt. Überträgt DECT200 in eine CCU Variable - Funktion SetCCUVariable mittels test.exe ohne Webmatic - Cleanup wget/curl Aufrufe. Client wird in Variable $WEBCLIENT konfiguriert

--- cuxdextra/FritzBox.sh
+++ cuxdextra/FritzBox.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Michael Leinich 14 Juni 2013 - 22:00
 # FritzBox.sh
-# Version 0.4.0
-# - replace WGET with Cuxd curl
-# - read configuration from FritzBox.conf
+# Version 0.5.0
+# https://github.com/Tscherno/Fritzbox.sh
 # ----------------------------------------------------------------------
 
 CPWMD5=./cpwmd5
@@ -18,6 +16,9 @@ FRITZHOME="/home/home.lua"
 TEMPFile="/var/tmp/FritzBox_tempfile.txt"
 CURLFile=""
 ANRUFLIST="/var/tmp/FritzBox_anruferliste.csv"
+
+# Wie werden die Webseiten aufgerufen
+WEBCLIENT="../curl -s"
 
 # Wohin soll geloggt werden
 Debug=""
@@ -47,12 +48,12 @@ PerformPOST(){
 	if [ "$2" = "GET" ]; then
 		Debugmsg=$Debugmsg"GET  : $1 \n"
 		Debugmsg=$Debugmsg"GET  : URL $URL?$1 \n"
-		../curl -s "$URL?$1" >$loggingfile
+		$WEBCLIENT "$URL?$1" >$loggingfile
 		Debugmsg=$Debugmsg"GET : Abgesendet \n"
 	else
 		Debugmsg=$Debugmsg"POST : $1 \n"
 		Debugmsg=$Debugmsg"POST : $URL \n"
-		../curl -s -d "$1" "$URL" >$loggingfile
+		$WEBCLIENT -d "$1" "$URL" >$loggingfile
 		Debugmsg=$Debugmsg"POST : Abgesendet \n"
 	fi
 }
@@ -80,7 +81,7 @@ $Debugmsg" > $Debug
 
 LOGIN(){
 	# Downlod Login XML nach TempFile
-	../curl -s "$FritzBoxURL$FRITZLOGIN">$TEMPFile
+	$WEBCLIENT "$FritzBoxURL$FRITZLOGIN">$TEMPFile
 	SessionInfoChallenge=$(sed -n '/.*<Challenge>\([^<]*\)<.*/s//\1/p' $TEMPFile)
 	SessionInfoSID=$(sed -n '/.*<SID>\([^<]*\)<.*/s//\1/p' $TEMPFile)
 	Debugmsg=$Debugmsg"LOGIN: Challenge $SessionInfoChallenge \n"
@@ -93,7 +94,7 @@ LOGIN(){
 		Debugmsg=$Debugmsg"LOGIN: login senden und SID herausfischen \n"
 		GETDATA="?username=$Username&response=$RESPONSE"
 		Debugmsg=$Debugmsg"LOGIN: $GETDATA \n"
-		../curl -s "$FritzBoxURL$FRITZLOGIN$GETDATA">$TEMPFile
+		$WEBCLIENT "$FritzBoxURL$FRITZLOGIN$GETDATA">$TEMPFile
 		SID=$(sed -n '/.*<SID>\([^<]*\)<.*/s//\1/p' $TEMPFile)
 		rm $TEMPFile
 	else
@@ -107,6 +108,14 @@ LOGIN(){
 		Debugmsg=$Debugmsg"LOGIN: Gueltige SID: $SID \n"
 	fi
 }
+
+SetCCUVariable(){
+	Debugmsg=$Debugmsg"SetCCUVariable $1 $2 \n"
+	Debugmsg=$Debugmsg"http://$HOMEMATIC:8181/test.exe?Status=dom.GetObject%28%27"$1"%27%29.State%28"$2"%29 \n"
+	
+	$WEBCLIENT "http://$HOMEMATIC:8181/test.exe?Status=dom.GetObject%28%27"$1"%27%29.State%28"$2"%29"
+}
+
 
 # Lese CONFIGFILE und ersetzen KEY: Value
 eval `sed '/^ *#/d;s/:/ /;' < "$CONFIGFILE" | while read key val
@@ -125,6 +134,7 @@ Debugmsg=$Debugmsg"Parameter FRITZWEBCM  = $FRITZWEBCM \n"
 Debugmsg=$Debugmsg"Parameter FRITZHOME   = $FRITZHOME \n"
 Debugmsg=$Debugmsg"Parameter TEMPFile    = $TEMPFile \n"
 Debugmsg=$Debugmsg"Parameter CURLFile    = $CURLFile \n"
+Debugmsg=$Debugmsg"Parameter WEBCLIENT    = $WEBCLIENT \n"
 Debugmsg=$Debugmsg"Parameter ANRUFLIST   = $ANRUFLIST \n"
 Debugmsg=$Debugmsg"Parameter Debug       = $Debug \n"
 Debugmsg=$Debugmsg"Parameter FritzBoxURL = $FritzBoxURL \n"
@@ -144,7 +154,7 @@ case $1 in
 	"test")  		LOGIN
 					suche="true"
 					linie=\"DECT\"
-					string=$(wget -O - "$FritzBoxURL$FRITZHOME?sid=$SID" 2>/dev/null | grep $linie )
+					string=$($WEBCLIENT "$FritzBoxURL$FRITZHOME?sid=$SID" 2>/dev/null | grep $linie )
 					if echo "$string" | egrep -q "true" ; then
 						echo "TRUE: $string"
 					else
@@ -176,13 +186,19 @@ case $1 in
 	"DECT200Status") LOGIN
 					PerformPOST "sid=$SID&command=AllOutletStates&xhr=0" "POST" "/net/home_auto_query.lua" "DECT200Status0.txt"
 					PerformPOST "sid=$SID&command=EnergyStats_10&id=$2&xhr=0" "POST" "/net/home_auto_query.lua" "DECT200ENERGIE0.txt";;
+	"DECT200Energie") LOGIN
+					Debugmsg=$Debugmsg"DECT200Energie $2 \n"
+					Debugmsg=$Debugmsg"$FritzBoxURL/net/home_auto_query.lua?sid=$SID&command=EnergyStats_10&id=$2&xhr=0 \n"
+					MM_Value_Power=$($WEBCLIENT "$FritzBoxURL/net/home_auto_query.lua?sid=$SID&command=EnergyStats_10&id=$2&xhr=0" | sed -e 's/[{}]/''/g' | awk -v RS=',"' -F: '/^MM_Value_Power/ {print $2}' | sed -e 's/"//g' | sed -e 's/ //g')
+					Debugmsg=$Debugmsg"MM_Value_Power von $2 = $MM_Value_Power \n"
+					SetCCUVariable $3 $MM_Value_Power
+					;;
 	"Anrufliste") 	LOGIN
-					wget -O - "$FritzBoxURL/fon_num/foncalls_list.lua?sid=$SID&csv=" >$ANRUFLIST 
-					echo "Step3 : Anrufliste runterladen" ;;
+					$WEBCLIENT "$FritzBoxURL/fon_num/foncalls_list.lua?sid=$SID&csv="  "$FritzBoxU RL/fon_num/foncalls_list.lua?sid=$SID&csv=" >$ANRUFLIST 
+					;;
 	"Anrufliste2CCU")
 					LOGIN
-					wget -O - "$FritzBoxURL/fon_num/foncalls_list.lua?sid=$SID&csv=" >$ANRUFLIST 
-					echo "Step3 : Anrufliste runterladen"
+					$WEBCLIENT "$FritzBoxURL/fon_num/foncalls_list.lua?sid=$SID&csv=" >$ANRUFLIST 
 					out="<table id='fritz'>"
 					count=0
 					anzahl=`expr $3 + 1`
@@ -198,15 +214,14 @@ case $1 in
 							nebenstelle=`echo "$line" | cut -f5 -d';'`
 							eigene=`echo "$line" | cut -f6 -d';'`
 							dauer=`echo "$line" | cut -f7 -d';'`
-							echo "DATEN: $count - $rufnummer - $dauer"
 							out=$out"<tr><td class='fritz_"$typ"'/><td>"$datum"</td><td>"$name"</td><td>"$rufnummer"</td><td>"$nebenstelle"</td><!--<td>"$eigene"</td>--><td>"$dauer"</td></tr>"
 						fi
 						count=`expr $count + 1`
 					done < $ANRUFLIST
 					out=$out"</table>"
 					urlencode=$(echo "$out" | sed -e 's/%/%25/g' -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\$/%24/g' -e 's/\&/%26/g' -e 's/'\''/%27/g' -e 's/(/%28/g' -e 's/)/%29/g' -e 's/\*/%2a/g' -e 's/+/%2b/g' -e 's/,/%2c/g' -e 's/-/%2d/g' -e 's/\./%2e/g' -e 's/\//%2f/g' -e 's/:/%3a/g' -e 's/;/%3b/g' -e 's//%3e/g' -e 's/?/%3f/g' -e 's/@/%40/g' -e 's/\[/%5b/g' -e 's/\\/%5c/g' -e 's/\]/%5d/g' -e 's/\^/%5e/g' -e 's/_/%5f/g' -e 's/`/%60/g' -e 's/{/%7b/g' -e 's/|/%7c/g' -e 's/}/%7d/g' -e 's/~/%7e/g')
-					wget -O - "http://$HOMEMATIC/addons/webmatic/cgi/set.cgi?id=$2&value=$urlencode"
-					echo "Step3 : Anrufliste an CCU gesendet";;
+					$WEBCLIENT "http://$HOMEMATIC/addons/webmatic/cgi/set.cgi?id=$2&value=$urlencode"
+					;;
 	"reboot") 		LOGIN
 					PerformPOST "logic:command/reboot=../gateway/commands/saveconfig.html&sid=$SID" "POST" 
 					PerformPOST "security:command/logout=1&sid=$SID" "POST";;
@@ -224,6 +239,7 @@ case $1 in
 					Debugmsg=$Debugmsg"        ./FritzBox.sh RUFUMLEITUNG [0|1|2|3(Rufumleistung)] [0|1] \n"
 					Debugmsg=$Debugmsg"        ./FritzBox.sh Diversity [0|1|2|3(Rufumleistung)] [0|1] \n"
 					Debugmsg=$Debugmsg"        ./FritzBox.sh DECT200 [16|17|18|19] [0|1] \n"
+					Debugmsg=$Debugmsg"        ./FritzBox.sh DECT200Energie [Nummer des Aktors:16|17|18|19] [Name der Variable in der CCU] - Beispiel: FritzBox.sh DECT200Energie 16 DECT200 \n"
 					Debugmsg=$Debugmsg"        ./FritzBox.sh Anrufliste \n"
 					Debugmsg=$Debugmsg"        ./FritzBox.sh Anrufliste2CCU [0000(HOMEMATIC Webmatic SYSVAR ID)] [Anzahl Eintraege] \n"
 					Debugmsg=$Debugmsg"        ./FritzBox.sh reboot \n"
